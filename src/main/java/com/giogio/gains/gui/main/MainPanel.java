ed08a91b1515ca80c47cfdbd266e8805f775fbfc
DESKTOP-K4RTMJM\Rodrigo <rodrigogrilledam@gmail.com>
2021-12-10 13:47:17 +0100

    Modificaciones en el mainPanel

--- src/main/java/com/giogio/gains/gui/main/MainPanel.java
+++ src/main/java/com/giogio/gains/gui/main/MainPanel.java
@@ -15,56 +15,90 @@ import java.awt.SystemColor;
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.FlowLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import javax.swing.JTextPane;
 
 public class MainPanel extends JPanel {
-	private JPanel WorkoutPanel;
-	private JPanel TrainPanel;
-	private JPanel GraphicPanel;
-	private JPanel TablePanel;
+	private JPanel panel;
+	private Component verticalStrut;
+	private JPanel panel_1;
+	private JPanel exercicePanel;
+	private JPanel tablePanel;
+	private JPanel workoutPanel;
+	private JPanel statisticsPanel;
+	private JLabel lblNewLabel;
+	private JLabel lblTablas;
+	private JLabel lblEntreno;
+	private JLabel lblEstadisticas;
 	/**
 	 * Create the panel.
 	 */
 	public MainPanel(final MainFrame frame) {
-		setBounds(new Rectangle(0, 0, 869, 605));
-		setLayout(null);
+		setBounds(new Rectangle(0, 0, 1082, 677));
+		setLayout(new BorderLayout(0, 0));
+		setLayout(new BorderLayout(0, 0));
 		
-		WorkoutPanel = new JPanel();
-		WorkoutPanel.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseMoved(MouseEvent e) {
-//				WorkoutPanel.setBackground(Color.LIGHT_GRAY);
-//				TrainPanel.setBackground(new Color(240,240,240));
-//				GraphicPanel.setBackground(new Color(240,240,240));
-//				TablePanel.setBackground(new Color(240,240,240));
-			}
-		});
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		add(WorkoutPanel);
-		WorkoutPanel.setLayout(new BorderLayout(0, 0));
+		panel = new JPanel();
+		add(panel, BorderLayout.NORTH);
 		
-		JButton btnNewButton_1 = new JButton("Ejercicios");
-		WorkoutPanel.add(btnNewButton_1);
+		verticalStrut = Box.createVerticalStrut(60);
+		panel.add(verticalStrut);
 		
-		TablePanel = new JPanel();
-		add(TablePanel);
-		TablePanel.setLayout(new BorderLayout(0, 0));
+		panel_1 = new JPanel();
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
 		
-		JButton btnNewButton = new JButton("Tablas");
-		TablePanel.add(btnNewButton);
+		exercicePanel = new JPanel();
+		exercicePanel.setBorder(new MatteBorder(1, 1, 1, 0, (Color) Color.DARK_GRAY));
+		panel_1.add(exercicePanel);
+		exercicePanel.setLayout(null);
 		
-		TrainPanel = new JPanel();
-		add(TrainPanel);
-		TrainPanel.setLayout(new BorderLayout(0, 0));
+		lblNewLabel = new JLabel("Ejercicios");
+		lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(45, 294, 179, 14);
+		exercicePanel.add(lblNewLabel);
 		
-		JButton btnNewButton_2 = new JButton("Entreno");
-		TrainPanel.add(btnNewButton_2, BorderLayout.CENTER);
+		tablePanel = new JPanel();
+		tablePanel.setBorder(new MatteBorder(1, 1, 1, 0, (Color) Color.DARK_GRAY));
+		panel_1.add(tablePanel);
+		tablePanel.setLayout(null);
 		
-		GraphicPanel = new JPanel();
-		add(GraphicPanel);
-		GraphicPanel.setLayout(new BorderLayout(0, 0));
+		lblTablas = new JLabel("Tablas");
+		lblTablas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTablas.setBounds(45, 294, 179, 14);
+		tablePanel.add(lblTablas);
 		
-		JButton btnNewButton_3 = new JButton("Estadísticas");
-		GraphicPanel.add(btnNewButton_3, BorderLayout.CENTER);
+		workoutPanel = new JPanel();
+		workoutPanel.setBorder(new MatteBorder(1, 1, 1, 0, (Color) Color.DARK_GRAY));
+		panel_1.add(workoutPanel);
+		workoutPanel.setLayout(null);
+		
+		lblEntreno = new JLabel("Entreno");
+		lblEntreno.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEntreno.setBounds(45, 294, 179, 14);
+		workoutPanel.add(lblEntreno);
+		
+		statisticsPanel = new JPanel();
+		statisticsPanel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.DARK_GRAY));
+		panel_1.add(statisticsPanel);
+		statisticsPanel.setLayout(null);
+		
+		lblEstadisticas = new JLabel("Estadisticas");
+		lblEstadisticas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEstadisticas.setBounds(45, 294, 179, 14);
+		statisticsPanel.add(lblEstadisticas);
 
 	}
 }
